810d14d4e17b6821e1b4c44067d1cc73319cf760
SiriMhM <sirimhm@gmail.com>
2017-11-06 15:43:36 +0100

    change login page, add header element

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+//var request = require("request");
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,9 +10,27 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+/*
+
+app.post('/login', function(request, response) {
+    request({
+    uri: "http://www.cjihrig.com/development/php/hello_form.php",
+    method: "POST",
+    form: {
+      name: "Bob"
+    }
+  }, function(error, response, body) {
+    console.log(body);
+  });
+});
+*/
+
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
